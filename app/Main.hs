03508f0f5d5ffae44c1a597b6a6bb6cad94121e2
etachott <bfwaend@gmail.com>
2024-03-15 23:21:49 -0300

    Add simple argument validation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
+import System.Environment
+import System.Exit (exitWith, ExitCode (ExitFailure))
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = validateArgs >>= print
+
+validateArgs :: IO ()
+validateArgs = do
+    args <- getArgs
+    case args of
+        [_] -> return ()
+        _wrongSize -> putStrLn "Usage: ./computor \"equation\"" >> exitWith (ExitFailure 1)
